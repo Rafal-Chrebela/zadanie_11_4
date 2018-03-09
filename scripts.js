13fe6d3ad63406c6488c821e9faa8a45a64aae44
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-03-09 16:42:37 +0100

    Change scripts.js

--- scripts.js
+++ scripts.js
@@ -2,19 +2,19 @@ function Phone(brand, price, color) {
   this.brand = brand;
   this.price = price;
   this.color = color;
-
-  Phone.prototype.printInfo = function() {
-    console.log(
-      "The phone brand is " +
-        this.brand +
-        ", color is " +
-        this.color +
-        " and the price is " +
-        this.price +
-        "."
-    );
-  };
 }
+
+Phone.prototype.printInfo = function() {
+  console.log(
+    "The phone brand is " +
+      this.brand +
+      ", color is " +
+      this.color +
+      " and the price is " +
+      this.price +
+      "."
+  );
+};
 
 var iPhone6S = new Phone("Apple", 2250, "silver");
 var SamsungS7 = new Phone("Samsung", 2500, "black");
